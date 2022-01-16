c628455eccc8dd6c9f751633e4fd7aa4057c5182
CeeBeeL <c.blervacques@free.fr>
2022-01-16 20:32:47 +0100

    Rename AudiusAPI to Audius & add const EndPoint

--- src/audius.nim
+++ src/audius.nim
@@ -12,8 +12,10 @@
 
 import httpclient, json
 
+const EndPoint = "https://api.audius.co"
+
 type 
-    AudiusApi* = object
+    Audius* = object
         client: HttpClient
         headers: HttpHeaders
         appName: string
@@ -31,20 +33,20 @@ type
         downloadable*: bool
         user*: User
 
-proc newAudiusApi*(appName: string): AudiusApi =
+proc newAudius*(appName: string = "EXAMPLE_:PP"): Audius =
     result.headers = newHttpHeaders([("Accept", "application/json")])
     result.client = newHttpClient(headers = result.headers)
     result.appName = "&app_name=" & appName
-    let servers = parseJson(result.client.getContent("https://api.audius.co"))
+    let servers = parseJson(result.client.getContent(EndPoint))
     result.server = servers["data"][0].getStr & "/v1"
 
-proc getUser*(api: AudiusApi, id: string): User =
+proc getUser*(api: Audius, id: string): User =
     let user = parseJson(api.client.getContent(api.server & "/users/" & id ))
     result = to(user["data"], User)
 
-proc getTrack*(api: AudiusApi, id: string): Track =
+proc getTrack*(api: Audius, id: string): Track =
     let track = parseJson(api.client.getContent(api.server & "/tracks/" & id ))
     result = to(track["data"], Track)
 
-proc getStreamTrack*(api: AudiusApi, id: string): string =
+proc getStreamTrack*(api: Audius, id: string): string =
     result = api.client.getContent(api.server & "/tracks/" & id & "/stream" )
